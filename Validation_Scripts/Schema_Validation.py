1db0bd8113ec30b68ef681b94b09d5886a7616fe
JingleXDingle <jrubioanoceto@gmail.com>
2025-08-13 20:42:13 -0400

    Added validation schema

--- Validation_Scripts/Schema_Validation.py
+++ Validation_Scripts/Schema_Validation.py
@@ -1,1 +1,9 @@
 #This script validates that every YAML and JSON file containing Microsoft Sentinel detection rules adheres to the defined schema.
+import os
+import sys
+import json
+import yaml
+import logging
+import argparse
+import re
+from jsonschema import Draft7Validator
